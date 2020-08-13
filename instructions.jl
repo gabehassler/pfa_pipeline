b45767f0e790e0e1d93586d52d61641ff1a9f122
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-08-12 17:56:26 -0700

    small fix

--- instructions.jl
+++ instructions.jl
@@ -60,5 +60,5 @@ const BEAST_SEED = 666 # the number of the BEAST (set to -1 for a random seed)
 
 ## General modeling choices
 
-CONSTRAIN_LOADINGS = true # set to `true` to enforce the constraint that the
+const CONSTRAIN_LOADINGS = true # set to `true` to enforce the constraint that the
                           # first trait only loads onto the first factor
