174bea920ac991a1c7d902250dd7ae1ffb197c74
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-11-24 10:53:24 -0500

    can now have multiple instructions files

--- pipeline.jl
+++ pipeline.jl
@@ -2,9 +2,28 @@ push!(LOAD_PATH, @__DIR__)
 
 using PipelineFunctions, ImportVariables
 
+instructions_paths = String[]
+if length(ARGS) > 0
+    for file in ARGS
+        path1 = joinpath(@__DIR__, file)
+        if isfile(path1)
+            push!(instructions_paths, path1)
+        elseif isfile(file)
+            push!(instructions_paths, file)
+        else
+            error("could not find instructions file $file")
+        end
+    end
+else
+    push!(instructions_paths, joinpath(@__DIR__, "instructions.jl"))
+end
+
+
 try
-    vars = import_variables()
-    run_pipeline(vars)
+    for path in instructions_paths
+        vars = import_variables(path)
+        run_pipeline(vars)
+    end
 catch e
     cd(@__DIR__)
     throw(e)
