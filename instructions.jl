79256f92500af314ecfee059de452a74969043fd
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-01-28 10:59:26 -0800

    many files in instructions

--- instructions.jl
+++ instructions.jl
@@ -4,13 +4,27 @@
 ## variables you MUST specify
 ################################################################################
 
-name = "X" # this will be the folder name that the results are stored
+name = "cham_trees" # this will be the folder name that the results are stored
                     # in as well as the name of the final xml and log files
 
-data_filename = "data.csv" # csv file where the data are stored (must be in ./data directory)
-newick_filename = "newick.txt" # file where the newick tree is stored (must be in ./data directory)
-instructions_filename = "sim_selection.csv" # csv file with relevant instructions for model selection
-labels_filename = "metadata.csv" # stores labeling information (and order) for loadings plot
+data_filename = "Chamaeleonidae_data_181.csv" # csv file where the data are stored (must be in ./data directory)
+newick_filename = ["Chamaeleonidae_tree_1.nwk",
+                    "Chamaeleonidae_tree_2.nwk",
+                    "Chamaeleonidae_tree_3.nwk",
+                    "Chamaeleonidae_tree_4.nwk",
+                    "Chamaeleonidae_tree_5.nwk",
+                    "Chamaeleonidae_tree_6.nwk",
+                    "Chamaeleonidae_tree_7.nwk",
+                    "Chamaeleonidae_tree_8.nwk",
+                    "Chamaeleonidae_tree_9.nwk",
+                    "Chamaeleonidae_tree_10.nwk",
+                    "Chamaeleonidae_tree_11.nwk",
+                    "Chamaeleonidae_tree_12.nwk",
+                    "Chamaeleonidae_tree_13.nwk",
+                    "Chamaeleonidae_tree_14.nwk",
+                    "Chamaeleonidae_tree_15.nwk"] # file where the newick tree is stored (must be in ./data directory)
+instructions_filename = "dummy_selection.csv" # csv file with relevant instructions for model selection
+labels_filename = "Chamaeleonidae_labels_181.csv" # stores labeling information (and order) for loadings plot
 
 
 ################################################################################
@@ -18,8 +32,8 @@ labels_filename = "metadata.csv" # stores labeling information (and order) for l
 ################################################################################
 
 ## Which parts of the pipeline get run
-MAKE_SELECTION_XML = false # create selection xml files
-RUN_SELECTION_XML = false # run model selection xml files
+MAKE_SELECTION_XML = true # create selection xml files
+RUN_SELECTION_XML = true # run model selection xml files
 MAKE_FINAL_XML = true # make final xml file
 RUN_FINAL_XML = true # run final xml file
 PLOT_LOADINGS = true # make plot summarizing the loadings matrix
@@ -37,13 +51,13 @@ LIKELIHOOD_CHECK_COUNT = -1 # how many steps in the MCMC chain will BEAST
 
 
 ## Model selection variables
-REPEATS = 3 # how many cross-validation sets do you want to run
+REPEATS = 1 # how many cross-validation sets do you want to run
 SPARSITY = 0.1 # what proportion of data to withold for cross-validation
 SELECTION_BURNIN = 0.5 # burnin for model selection (i.e. the proportion of
                              # states that will be ignored when calculating the
                              # posterior mean predictive likelihood)
 
-SELECTION_STATISTIC = ["CLPD", "MLPD"] # specific statistic you're trying to maximize (or minimize)
+SELECTION_STATISTIC = ["MSE"] # specific statistic you're trying to maximize (or minimize)
                                 # Options are:
                                 #     1) "CLPD" - log predictive density conditional on observed data
                                 #     2) "MLPD" - marginal log predictive density
@@ -62,9 +76,9 @@ BEAST_SEED = -1 # the number of the BEAST (set to -1 for a random seed)
 
 
 ## General modeling choices
-CONSTRAIN_LOADINGS = false # set to `true` to enforce the constraint that the
+CONSTRAIN_LOADINGS = true # set to `true` to enforce the constraint that the
                           # first trait only loads onto the first factor
-SHRINK_LOADINGS = true # set to `true` to use a multiplicative gamma shrinkage
+SHRINK_LOADINGS = false # set to `true` to use a multiplicative gamma shrinkage
                         # prior on the loadings
 STANDARDIZE_DATA = true # set to `true` to standardize each trait to have
                         # mean 0 and variance 1
--- julia_setup.jl
+++ julia_setup.jl
@@ -2,3 +2,4 @@ using Pkg
 
 Pkg.add(["CSV", "DataFrames", "LinearAlgebra", "Statistics", "UnPack", "RCall"])
 Pkg.add(Pkg.PackageSpec(url="https://github.com/gabehassler/BeastUtils.jl.git"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/Julia-BEAST-utils/PosteriorSummary.jl.git"))
